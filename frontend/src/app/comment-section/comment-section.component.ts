144186abb2d6b7ff0ec1f10f40d4437b59ba07ff
Sebastian Marut <sebastian.marut@op.pl>
2019-10-25 18:27:11 +0200

    Comment section component added

---
+++ frontend/src/app/comment-section/comment-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-section',
+  templateUrl: './comment-section.component.html',
+  styleUrls: ['./comment-section.component.css']
+})
+export class CommentSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
