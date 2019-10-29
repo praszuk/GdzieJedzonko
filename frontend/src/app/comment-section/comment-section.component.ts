bb1799d6116c8da2538696bdb79efead93f7f4a8
Sebastian Marut <sebastian.marut@op.pl>
2019-10-29 12:58:24 +0100

    Comment section comleped

--- frontend/src/app/comment-section/comment-section.component.ts
+++ frontend/src/app/comment-section/comment-section.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Comment} from '../comment';
+import {User} from '../user';
 
 @Component({
   selector: 'app-comment-section',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment-section.component.css']
 })
 export class CommentSectionComponent implements OnInit {
-
+  comments: Comment [];
   constructor() { }
 
   ngOnInit() {
+    this.dummyData();
+  }
+
+  dummyData() {
+    this.comments = [{
+        id: 1,
+        user: {
+          id: 1,
+          email: 'test1@gmail.com',
+          password: 'test1',
+          firstName: 'testName1',
+          lastName: 'testSurName1',
+          birthDate: 'testdate1',
+          joinDate: 'testJoindate1'
+        },
+        comment: 'vitae, volutpat sit amet erat. Aenean ac magna laoreet, pellentesque diam eget, interdum mi. Cras sed eleifend neque. Nam sit amet eros sit amet lacus semper malesuada. Vestibulum a ante non lorem maximus rhoncus non ut risus. Etiam pellentesque neque non est dictum, non facilisis dui vestibulum. Praesent lobortis, urna vitae condimentum egestas, ipsum lacus viverra turpis, commodo cursus turpis sem non tellus. Duis placerat leo et diam sollicitudin malesuada. Suspendisse tempus accumsan aliquam. Maecenas nunc nisi, malesuada vel lorem nec, accumsan varius felis. Nam iaculis eros vitae imperdiet volutpat. Suspendisse orci sem, aliquet sit amet congue nec, euismod id nulla. Vestibulum scelerisque quam ultricies ligula mattis, vitae posuere mauris gravida. Aenean gravida facilisis arcu, ut venenatis lacus luctus nec. Sed vel euismod dui. Etiam turpis magna, volutpat vitae mollis et, rutrum non ',
+        creationDate: 'test creation date',},
+      {
+        id: 2,
+        user: {
+          id: 2,
+          email: 'test2@gmail.com',
+          password: 'test2',
+          firstName: 'testName2',
+          lastName: 'testSurName2',
+          birthDate: 'testdate2',
+          joinDate: 'testJoindate2'
+        },
+        comment: 'Test comment 2',
+        creationDate: 'test creation date',}
+  ];
   }
 
 }
