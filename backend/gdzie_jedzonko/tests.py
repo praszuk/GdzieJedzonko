27330f5bdcc91b95d72770e23c5f9d94a513ef0e
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-10-31 03:19:06 +0100

    Added tests for create new uer for password check.

--- backend/gdzie_jedzonko/tests.py
+++ backend/gdzie_jedzonko/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserModelTest(TestCase):
+
+    def test_create_user_and_check_is_password_hashed(self):
+        user = User.objects.create_user(email='test@test.com', password='123')
+        self.assertNotEqual(user.password, '123')
+        self.assertTrue(user.check_password('123'))
