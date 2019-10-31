eaa1ac6e5dee0e7ed70d71b10c016c382b69257b
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-10-31 04:49:01 +0100

    Added UserList view.

--- backend/gdzie_jedzonko/views.py
+++ backend/gdzie_jedzonko/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import User
+from .serializers import UserListSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
