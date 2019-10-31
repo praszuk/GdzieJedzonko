f19566ab05e77546ed09be8bd6df6ae73675ad06
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-10-31 03:58:01 +0100

    Enabled rest_framework in settings.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 
     'gdzie_jedzonko.apps.GdzieJedzonkoConfig',
 ]
