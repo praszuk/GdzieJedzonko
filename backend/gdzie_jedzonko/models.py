3757f249f25d0074e6aea9d350bd2cd6a20b3439
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-10-31 02:35:28 +0100

    Added user model class.

--- backend/gdzie_jedzonko/models.py
+++ backend/gdzie_jedzonko/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+
+    first_name = models.TextField(max_length=50, blank=True)
+    last_name = models.TextField(max_length=50, blank=True)
+    birth_date = models.DateField(blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
