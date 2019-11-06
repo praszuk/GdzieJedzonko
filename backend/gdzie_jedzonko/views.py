4244d5cd2b33c772877f6e32bf08e22a9e8509b9
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-11-06 23:07:56 +0100

    Added restriction to UserList view.
    Set permission_classes to IsAuthenticated and IsAdminUser.

--- backend/gdzie_jedzonko/views.py
+++ backend/gdzie_jedzonko/views.py
@@ -1,9 +1,12 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .models import User
+from .permissions import IsAdminUser
 from .serializers import UserListSerializer
 
 
 class UserList(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, IsAdminUser)
     queryset = User.objects.all()
     serializer_class = UserListSerializer
