fc1764809f0fcdcc6654a3dc54a6840fe62d5cc8
Sebastian Marut <sebastian.marut@op.pl>
2019-11-07 14:49:23 +0100

    Authorization service template added

--- frontend/src/app/auth/services/auth-service.ts
+++ frontend/src/app/auth/services/auth-service.ts
@@ -1,9 +1,38 @@
 import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {User} from "../../user";
+import {Token} from "../token.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly ACCESS_TOKEN = "access";
+  private readonly REFRESH_TOKEN = "refresh";
+  private user: User;
+
+  constructor(private http: HttpClient) {}
+
+
+  login() {
+
+  }
+
+  logout() {
+
+  }
+
+  isLoggedIn() {
+
+  }
+
+  refreshToken() {
+
+  }
+
+  getToken() {
+
+  }
 
 }
