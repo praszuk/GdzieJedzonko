16d7dc3b68fc95b95f6dc36c56413d789ab99ef6
Sebastian Marut <sebastian.marut@op.pl>
2019-11-07 02:02:25 +0100

    Login form view finished

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private loginFormBuilder: FormBuilder ) {}
 
   ngOnInit() {
+    this.loginForm = this.loginFormBuilder.group({
+      email: [''],
+      password: ['']
+    });
   }
 
+  onSubmit(credentials: any) {
+    console.log(credentials)
+  }
 }
