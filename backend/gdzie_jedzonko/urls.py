dbccea5c05de3d694ea716d1ababb797c81bfd8f
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-11-08 01:33:05 +0100

    Added user-detail url to the UserViewSet.

--- backend/gdzie_jedzonko/urls.py
+++ backend/gdzie_jedzonko/urls.py
@@ -11,6 +11,10 @@ user_list = UserViewSet.as_view({
     'get': 'list',
 })
 
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
 urlpatterns = [
     path('sessions/token/',
          MyTokenObtainPairView.as_view(),
@@ -20,5 +24,6 @@ urlpatterns = [
          TokenRefreshView.as_view(),
          name='token_refresh'),
 
-    path('users/', user_list, name='user-list')
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail')
 ]
