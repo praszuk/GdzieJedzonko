92eec3d0072a1aef23f046a4552337c888f5a67c
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-11-09 19:50:30 +0100

    Added test for auth. user but not owner.

--- backend/gdzie_jedzonko/tests.py
+++ backend/gdzie_jedzonko/tests.py
@@ -186,3 +186,19 @@ class DeleteUserTest(BaseViewTest):
             reverse('gdzie_jedzonko:user-detail', args=[1])
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_authenticated_user_but_not_owner(self):
+        user2 = User.objects.get(email=self.USERS[1]['email'])
+
+        credentials = self.generate_credentials(
+            self.USERS[0]['email'],
+            self.USERS[0]['password']
+        )
+        self.client.credentials(HTTP_AUTHORIZATION=credentials)
+
+        response = self.client.delete(
+            reverse('gdzie_jedzonko:user-detail', args=[user2.id])
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertIsNotNone(User.objects.get(email=self.USERS[1]['email']))
