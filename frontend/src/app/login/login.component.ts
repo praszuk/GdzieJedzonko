c7486f9023506d4ebe5c8e8bc027e20e35697d70
Sebastian Marut <sebastian.marut@op.pl>
2019-11-09 18:54:19 +0100

    Login page and navbar functionality with JWT finished

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthService} from "../auth/services/auth-service";
 
 @Component({
   selector: 'app-login',
@@ -8,7 +10,10 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
-  constructor(private loginFormBuilder: FormBuilder ) {}
+  private isError: boolean;
+  constructor(private loginFormBuilder: FormBuilder,
+              private authService: AuthService,
+              private router: Router) {}
 
   ngOnInit() {
     this.loginForm = this.loginFormBuilder.group({
@@ -17,7 +22,17 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  onSubmit(credentials: any) {
-    console.log(credentials)
+  onSubmit(credentials: {email: string; password: string}) {
+    this.authService.login(credentials).subscribe(
+      (response) => {
+          if(response)
+            this.router.navigate(['/home']);
+          else
+            this.isError = true;
+      },
+      (error) =>{
+        console.log(error)
+
+      })
   }
 }
