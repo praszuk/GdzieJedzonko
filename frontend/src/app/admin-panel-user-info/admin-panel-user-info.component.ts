2d13fd777aea21f77954e66f2ec190f8adb6260b
Adrian <miciukiewicza@gmail.com>
2019-11-19 19:08:33 +0100

    Edited page of admin-panel-user-info

--- frontend/src/app/admin-panel-user-info/admin-panel-user-info.component.ts
+++ frontend/src/app/admin-panel-user-info/admin-panel-user-info.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-panel-user-info.component.css']
 })
 export class AdminPanelUserInfoComponent implements OnInit {
+  email: string;
+  // tslint:disable-next-line:variable-name
+  first_name: string;
+  // tslint:disable-next-line:variable-name
+  last_name: string;
+  // tslint:disable-next-line:variable-name
+  birth_date;
+  // tslint:disable-next-line:variable-name
+  join_date;
+
+
 
   constructor() { }
 
   ngOnInit() {
+    this.testValues();
+  }
+
+  testValues() {
+    this.email = 'testowymail@wp.pl';
+    // tslint:disable-next-line:max-line-length
+    this.first_name = 'Pan';
+    this.last_name = 'Pawel';
+    this.birth_date = '2000-01-01';
+    this.join_date = '2019-01-01';
   }
 
 }
