630042a462a2d912389182bc72f2925507d6faec
Sebastian Marut <sebastian.marut@op.pl>
2019-11-24 22:04:42 +0100

    Moved config variables from config.ts to dedicated angular files

--- frontend/src/app/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  apiUrl: 'http://0.0.0.0:8000',
-  loginUrl: '/api/sessions/token/',
-  refreshUrl: '/api/sessions/refresh/',
-  registerUrl: '/api/users/',
-};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://0.0.0.0:8000',
+  loginUrl: '/api/sessions/token/',
+  refreshUrl: '/api/sessions/refresh/',
+  registerUrl: '/api/users/'
 };
 
 /*
