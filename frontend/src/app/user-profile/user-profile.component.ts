78c19b06532d07f22a8e0fa9c3ccaf044ad91e84
Sebastian Marut <sebastian.marut@op.pl>
2019-11-24 13:30:32 +0100

    User profile view finished

--- frontend/src/app/user-profile/user-profile.component.ts
+++ frontend/src/app/user-profile/user-profile.component.ts
@@ -7,7 +7,7 @@ import {User} from '../user';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
+export class UserProfileComponent implements OnInit, OnDestroy {
  subsription;
  userId: string;
  user: User;
@@ -18,6 +18,11 @@ export class UserProfileComponent implements OnInit {
         this.userId = params.get('id');
       }
     );
+    this.getUserById(this.userId);
+  }
+
+  getUserById(userId){
+    this.user = {id:1,firstName:"first",email:"a@w.pl",lastName:"last",birthDate:"2000-11-11",joinDate:"2019-11-11",password:"11"};
   }
 
   ngOnDestroy() {
