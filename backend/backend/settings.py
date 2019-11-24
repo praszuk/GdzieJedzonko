3e9bd206c839067707dc321bfee3abeeffb653ae
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-11-24 21:03:07 +0100

    Changed ALLOWED_HOSTS to value from env vars.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if 'DEV' in os.environ and os.environ['DEV'] == '1' else False
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = os.environ.get('BACKEND_ALLOWED_HOST', '0.0.0.0').split(',')
 
 
 # Application definition
