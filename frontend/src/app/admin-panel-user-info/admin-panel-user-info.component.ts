96d61ad7787ac685fb58a5eba6468b104b45536c
Adrian <miciukiewicza@gmail.com>
2019-11-26 00:03:37 +0100

    Changed admin-panel-user-info to use user class
    Deleted unnecessary test values, added user class to admin-panel-user-info

--- frontend/src/app/admin-panel-user-info/admin-panel-user-info.component.ts
+++ frontend/src/app/admin-panel-user-info/admin-panel-user-info.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from '../user';
 
 @Component({
   selector: 'app-admin-panel-user-info',
@@ -6,11 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-panel-user-info.component.css']
 })
 export class AdminPanelUserInfoComponent implements OnInit {
-  email: string;
-  first_name: string;
-  last_name: string;
-  birth_date;
-  join_date;
+  @Input() user: User;
   roles: string[];
   selectedRole = 'Rola';
 
@@ -18,19 +15,10 @@ export class AdminPanelUserInfoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.testValues();
+    this.roles = ['1', '2', '3'];
   }
 
-  testValues() {
-    this.email = 'testowymail@wp.pl';
-    this.first_name = 'Pan';
-    this.last_name = 'Pawel';
-    this.birth_date = '2000-01-01';
-    this.join_date = '2019-01-01';
-    this.roles = ['Moderator', 'Admin', 'User'];
-  }
-
-  selected(role: string){
+  selected(role: string) {
     this.selectedRole = role;
   }
 
