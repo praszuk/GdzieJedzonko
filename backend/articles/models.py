b584d15d737c75bf9a6525c02f4bd2352629e9d1
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-11-27 00:23:36 +0100

    Added Article model.

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100, unique=True, blank=False)
+    content = models.TextField(max_length=3000, blank=False)
+    creation_date = models.DateTimeField(auto_now=True)
