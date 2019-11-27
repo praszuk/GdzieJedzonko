532a648af93c0420db35153d9ca53e2404705177
Sebastian Marut <sebastian.marut@op.pl>
2019-11-27 20:26:29 +0100

    Article service added

---
+++ frontend/src/app/services/article/article.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Article} from '../../models/article.model';
+import {Observable, of, throwError} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {catchError, mapTo} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  constructor(private http: HttpClient) {}
+
+  newReview(article: {title: string, content: string}): Observable<string> {
+    return this.http.post(`${environment.apiUrl}${environment.newArticle}`, article)
+      .pipe(
+        mapTo('success'),
+        catchError((err) => {
+          return throwError(err.title);
+        })
+      );
+  }
+}
