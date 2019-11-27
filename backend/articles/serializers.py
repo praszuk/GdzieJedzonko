8c0c7eaa50ff51e0544e188d6428495b0f3c7b95
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-11-27 05:01:06 +0100

    Added UserSerializer for/and ArticleSerizalier for ArticleViewSet.

--- backend/articles/serializers.py
+++ backend/articles/serializers.py
@@ -1,1 +1,21 @@
 from rest_framework import serializers
+
+from .models import Article
+from users.models import User
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        depth = 1
+        fields = ('id', 'first_name', 'last_name')
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True, many=False)
+
+    class Meta:
+        model = Article
+        depth = 1
+        fields = '__all__'
+        read_only_fields = ('id', 'creation_date')
