8ecc9f47674a3ed2c4f1b755d66ddf0360d0ff51
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-11-27 05:36:12 +0100

    Added test everyone can get detail.

--- backend/articles/tests.py
+++ backend/articles/tests.py
@@ -1,1 +1,40 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+
+from django.urls import reverse
+
+from .models import Article
+from .serializers import ArticleSerializer
+from users.models import User, Role
+
+
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+
+class GetDetailTest(BaseViewTest):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(
+            email='test@gdziejedzonko.pl',
+            password='password1234',
+            first_name='John',
+            last_name='Smith',
+            role=Role.USER
+        )
+        Article.objects.create(
+            title='Test title',
+            content='Content of the article',
+            user=user
+        )
+
+    def test_everyone_can_get_detail(self):
+        expected = Article.objects.all()[0]
+        response = self.client.get(
+            reverse('articles:article-detail', args=[expected.id])
+        )
+        serialized = ArticleSerializer(expected, many=False)
+
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
