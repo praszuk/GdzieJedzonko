0aab2b64b73d5e0e91ca1f16677ac99834a68f9c
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-11-27 01:34:35 +0100

    Added user association.

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from users.models import User
+
 
 class Article(models.Model):
     title = models.CharField(max_length=100, unique=True, blank=False)
     content = models.TextField(max_length=3000, blank=False)
     creation_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
