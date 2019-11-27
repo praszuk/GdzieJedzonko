015536ddf7533e4fe6bcc80a28e957d0b03fc85f
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-11-27 17:05:53 +0100

    Added url article-list to ArticleViewSet.

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -10,7 +10,11 @@ article_detail = ArticleViewSet.as_view({
     'get': 'retrieve',
 })
 
+article_list = ArticleViewSet.as_view({
+    'get': 'list',
+})
 
 urlpatterns = [
-    path('<int:pk>/', article_detail, name='article-detail')
+    path('', article_list, name='article-list'),
+    path('<int:pk>/', article_detail, name='article-detail'),
 ]
