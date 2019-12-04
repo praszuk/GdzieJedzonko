79abd5f11823294accfb68d8b27d01ef1f7459b1
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-12-04 23:19:21 +0100

    Added DEBUG mode for serving images in urls.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from users.urls import role_list
 
@@ -14,3 +16,9 @@ urlpatterns = [
     # Articles
     path('api/articles/', include('articles.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
