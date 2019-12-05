ebbe7611a215ceb5fcbd0642f5e1312a6c6bd9ae
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-12-05 01:26:58 +0100

    Added Image model.

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -3,8 +3,25 @@ from django.db import models
 from users.models import User
 
 
+class Image(models.Model):
+    image = models.ImageField()
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100, unique=True, blank=False)
     content = models.TextField(max_length=3000, blank=False)
     creation_date = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    thumbnail = models.OneToOneField(
+        Image,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='thumbnail'
+    )
+    images = models.ForeignKey(
+        Image,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='images'
+    )
