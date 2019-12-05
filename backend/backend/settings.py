c8f33d2943f3a6ba151b972a3d3576e684816b84
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-12-05 01:21:11 +0100

    Added django-cleanup plugin for media files.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_cleanup.apps.CleanupConfig',
 
     'articles.apps.ArticlesConfig',
     'authentication.apps.AuthenticationConfig',
