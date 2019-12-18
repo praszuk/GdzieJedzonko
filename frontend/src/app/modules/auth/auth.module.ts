0c3fd6e24cbda945fe529e33eca6c5ca32e977df
Sebastian Marut <sebastian.marut@op.pl>
2019-12-18 03:04:58 +0100

    Moved register and login page under auth module

--- frontend/src/app/modules/auth/auth.module.ts
+++ frontend/src/app/modules/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {LoginComponent} from "../../login/login.component";
-import {RegisterComponent} from "../../register/register.component";
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {RouterModule} from "@angular/router";
