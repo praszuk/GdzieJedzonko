d59e36e0c3c88e0e4d6fcacec46dd38d565efbe4
Sebastian Marut <sebastian.marut@op.pl>
2019-12-19 06:10:43 +0100

    Added custom toolbar to editor

--- frontend/src/app/components/new-review/new-review.component.ts
+++ frontend/src/app/components/new-review/new-review.component.ts
@@ -16,11 +16,32 @@ export class NewReviewComponent implements OnInit, OnDestroy {
   titleExists = false;
   subscription: Subscription = null;
 
+  toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+    [{ header: 1 }, { header: 2 }],               // custom button values
+    [{ list: 'ordered'}, { list: 'bullet' }],
+    [{ script: 'sub'}, { script: 'super' }],      // superscript/subscript
+    [{ indent: '-1'}, { indent: '+1' }],          // outdent/indent
+    [{ direction: 'rtl' }],                         // text direction
+
+    [{ size: ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+    [{ color: [] }, { background: [] }],          // dropdown with defaults from theme
+    [{ font: [] }],
+    [{ align: [] }],
+
+    ['clean']                                         // remove formatting button
+  ];
+
   editorStyle = {
-    height: '35vh'
+    height: '70vh'
   };
 
   modules = {
+    toolbar: this.toolbarOptions
   };
 
 
