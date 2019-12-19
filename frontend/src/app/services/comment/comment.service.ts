190305e28656210faa5b6ba9ebf189b422995e70
Sebastian Marut <sebastian.marut@op.pl>
2019-12-19 16:48:03 +0100

    Comment service added

---
+++ frontend/src/app/services/comment/comment.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Comment} from '../../models/comment.model';
+import {Observable, throwError} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {catchError} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private http: HttpClient) { }
+
+  createComment(articleId: number, comment: Comment): Observable<Comment> {
+    return this.http.post<Comment>(`${environment.apiUrl}${environment.articlesUrl}${articleId}${environment.commentsUrl}`,
+      comment)
+      .pipe(
+        catchError(
+          (err) => throwError(err)
+        )
+      );
+  }
+
+  getAllComments(articleId: number): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${environment.apiUrl}${environment.articlesUrl}${articleId}${environment.commentsUrl}`)
+      .pipe(
+        catchError(
+          (err) => throwError(err)
+        )
+      );
+  }
+}
