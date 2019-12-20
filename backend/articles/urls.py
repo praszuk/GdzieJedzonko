932a871fe8ce8dd7b0a49275d9b6d1ce32b7d8f9
Paweł Raszuk <pawel.raszuk@gmail.com>
2019-12-20 04:12:14 +0100

    Added url to deleting images from article.

--- backend/articles/urls.py
+++ backend/articles/urls.py
@@ -20,8 +20,17 @@ images_list = ImageViewSet.as_view({
     'post': 'create',
 })
 
+images_detail = ImageViewSet.as_view({
+    'delete': 'destroy',
+})
+
 urlpatterns = [
     path('', article_list, name='article-list'),
     path('<int:pk>/', article_detail, name='article-detail'),
     path('<int:article_id>/images/', images_list, name='images-list'),
+    path(
+        '<int:article_id>/images/<int:image_id>/',
+        images_detail,
+        name='images-detail'
+    ),
 ]
