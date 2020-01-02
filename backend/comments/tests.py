00a40d9d4dc3bc193237bbab560b4793a4c8dbc0
Paweł Raszuk <pawel.raszuk@gmail.com>
2020-01-02 06:32:48 +0100

    Added test everyone can get list of comments.

--- backend/comments/tests.py
+++ backend/comments/tests.py
@@ -1,6 +1,13 @@
+from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 
+from django.urls import reverse
+
+from articles.models import Article
 from users.models import User, Role
+
+from .models import Comment
+from .serializers import CommentSerializer
 
 
 class BaseViewTest(APITestCase):
@@ -97,3 +104,52 @@ class BaseViewTest(APITestCase):
         for admin in self.ADMINS:
             User.objects.create_user(**admin)
 
+
+class GetAllCommentsTest(BaseViewTest):
+
+    def setUp(self):
+        super().setUp()
+        self.user = User.objects.create_user(
+            email='user1@gdziejedzonko.pl',
+            password='password1234',
+            first_name='John',
+            last_name='Smith',
+            role=Role.USER
+        )
+
+        self.article = Article.objects.create(
+            title='Title',
+            content=self.article_content,
+            user=self.user
+        )
+
+        self.comment1 = Comment.objects.create(
+            content=self.comment_content1,
+            article=self.article,
+            user=self.user
+        )
+        self.comment2 = Comment.objects.create(
+            content=self.comment_content2,
+            article=self.article,
+            user=self.user
+        )
+        self.comment3 = Comment.objects.create(
+            content=self.comment_content3,
+            article=self.article,
+            user=self.user
+        )
+
+    def test_everyone_can_get_list_of_comments(self):
+        expected = Comment.objects.filter(
+            article=self.article
+        ).order_by('-creation_date')
+        response = self.client.get(
+            reverse(
+                'articles:comments:comment-list',
+                kwargs={'article_id': self.article.id}
+            )
+        )
+        serialized = CommentSerializer(expected, many=True)
+
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
