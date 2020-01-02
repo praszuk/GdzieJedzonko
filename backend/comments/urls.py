4f157e2b2e244e00d95b8cabf03bb5e502c80857
Paweł Raszuk <pawel.raszuk@gmail.com>
2020-01-02 05:31:18 +0100

    Added url for comments.
    Routing comments from articles to comments app.

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
+from .views import CommentViewSet
+
 
 app_name = 'comments'
 
 
-urlpatterns = [
+comment_list = CommentViewSet.as_view({
+    'get': 'list',
+})
 
+urlpatterns = [
+    path('', comment_list, name='comment-list'),
 ]
