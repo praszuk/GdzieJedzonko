10001e62ff21b29b712f275b34be5d59b18d6066
Paweł Raszuk <pawel.raszuk@gmail.com>
2020-01-02 06:35:22 +0100

    Added CommentViewSet for listing comments.

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 
-# Create your views here.
+from articles.models import Article
+
+from .models import Comment
+from .serializers import CommentSerializer
+from .permissions import CommentArticlePermission
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.order_by('-creation_date')
+    serializer_class = CommentSerializer
+    permission_classes = (CommentArticlePermission, )
+
+    def get_queryset(self):
+        art = get_object_or_404(Article, id=self.kwargs['article_id'])
+
+        return Comment.objects.filter(article=art).order_by('-creation_date')
