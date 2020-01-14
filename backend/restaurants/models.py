ed302e0ff258e891dab2cf02a816c39c2c2d08ea
Paweł Raszuk <pawel.raszuk@gmail.com>
2020-01-14 02:30:07 +0100

    Added City model.

--- backend/restaurants/models.py
+++ backend/restaurants/models.py
@@ -7,3 +7,13 @@ class Location(models.Model):
 
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return f'lat: {self.lat}, lon: {self.lon}'
+
+
+class City(Location):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'name: {self.name}, ' + Location.__str__(self)
