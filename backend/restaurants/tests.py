eef88fcb2114a62eba966b69ec026499045a4111
Paweł Raszuk <pawel.raszuk@gmail.com>
2020-01-14 05:54:01 +0100

    Added test everyone can get list of cities.

--- backend/restaurants/tests.py
+++ backend/restaurants/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
 
-# Create your tests here.
+from django.urls import reverse
+
+from .models import City
+from .serializers import CitySerializer
+
+
+class GetAllCitiesTest(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+        City.objects.create(name='Berlin', lat='52.52000', lon='13.40495')
+        City.objects.create(name='Paris', lat='48.86471', lon='2.34901')
+        City.objects.create(name='Warsaw', lat='52.23704', lon='21.01753')
+
+    def test_everyone_can_get_list_of_cities(self):
+        expected = City.objects.all()
+        response = self.client.get(reverse('restaurants:cities-list'))
+        serialized = CitySerializer(expected, many=True)
+
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
