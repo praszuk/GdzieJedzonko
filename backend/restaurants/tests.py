ea65c970f44c683de4f49f3dee9a21d41052aa27
Paweł Raszuk <pawel.raszuk@gmail.com>
2020-01-14 06:30:18 +0100

    Added test only admin can create.

--- backend/restaurants/tests.py
+++ backend/restaurants/tests.py
@@ -3,8 +3,83 @@ from rest_framework.test import APIClient, APITestCase
 
 from django.urls import reverse
 
+from users.models import User, Role
+
 from .models import City
 from .serializers import CitySerializer
+
+
+class BaseViewTest(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+        self.USERS = [
+            {
+                'email': 'articleuser1@gdziejedzonko.pl',
+                'password': 'password1234',
+                'first_name': 'David',
+                'last_name': 'Davis'
+            },
+            {
+                'email': 'articleuser2@gdziejedzonko.pl',
+                'password': 'password1234',
+                'first_name': 'John',
+                'last_name': 'Smith',
+                'birth_date': '1978-02-12'
+            },
+        ]
+        self.MODS = [
+            {
+                'email': 'articlemod@gdziejedzonko.pl',
+                'password': 'password1234',
+                'first_name': 'Micheal',
+                'last_name': 'Johnson',
+                'birth_date': '1970-09-22',
+                'role': Role.MODERATOR
+            },
+        ]
+        self.ADMINS = [
+            {
+                'email': 'articleadmin@gdziejedzonko.pl',
+                'password': 'password1234',
+                'first_name': 'Adam',
+                'last_name': 'Williams',
+                'birth_date': '1970-09-22',
+                'role': Role.ADMIN
+            }
+        ]
+
+        for user in self.USERS:
+            User.objects.create_user(**user)
+
+        for mod in self.MODS:
+            User.objects.create_user(**mod)
+
+        for admin in self.ADMINS:
+            User.objects.create_user(**admin)
+
+
+    def generate_credentials(self, email: str, password: str):
+        """
+        Generating credentials string using token_obtain_pair endpoint.
+        :return: Credentials string in format 'Bearer <access token>'
+        :rtype: str
+        """
+        data = {'email': email, 'password': password}
+        response = self.client.post(
+            reverse('authentication:token_obtain_pair'),
+            data=data
+        )
+
+        return 'Bearer ' + response.data['access']
+
+    def auth_user(self, user: dict):
+        """
+        Helper method for generating credentials
+        :param user: user_data with keys (it has to contain email and password)
+        """
+        auth_data = self.generate_credentials(user['email'], user['password'])
+        self.client.credentials(HTTP_AUTHORIZATION=auth_data)
 
 
 class GetAllCitiesTest(APITestCase):
@@ -24,3 +99,32 @@ class GetAllCitiesTest(APITestCase):
         self.assertEqual(response.data, serialized.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+
+class CreateCityTest(BaseViewTest):
+
+    def test_only_admin_can_create(self):
+        city_data = {'name': 'Warsaw', 'lat': '52.23704', 'lon': '21.01753'}
+        url = reverse('restaurants:cities-list')
+
+        # Not authenticated
+        response = self.client.post(url, city_data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        # User
+        self.auth_user(self.USERS[0])
+        response = self.client.post(url, city_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Mod
+        self.auth_user(self.MODS[0])
+        response = self.client.post(url, city_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Admin
+        self.auth_user(self.ADMINS[0])
+        response = self.client.post(url, city_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            CitySerializer(City.objects.all()[0], many=False).data,
+            response.data
+        )
