ddf42bb649b410e591f117b0ece36cebb3e3d42c
Paweł Raszuk <pawel.raszuk@gmail.com>
2020-01-14 01:49:53 +0100

    Added Location model.

--- backend/restaurants/models.py
+++ backend/restaurants/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    lat = models.DecimalField(max_digits=8, decimal_places=5)
+    lon = models.DecimalField(max_digits=8, decimal_places=5)
+
+    class Meta:
+        abstract = True
