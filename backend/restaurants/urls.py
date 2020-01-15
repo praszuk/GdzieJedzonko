e947f4486e301449a9900f5610adb9d5e68ef78d
Paweł Raszuk <pawel.raszuk@gmail.com>
2020-01-15 06:33:05 +0100

    Added url.

--- backend/restaurants/urls.py
+++ backend/restaurants/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CityViewSet, RestaurantViewSet
+from .views import CityViewSet, RestaurantViewSet, RestaurantListPendingViewSet
 
 
 app_name = 'restaurants'
@@ -23,8 +23,17 @@ restaurants_list = RestaurantViewSet.as_view({
     'get': 'list'
 })
 
+restaurants_list_pending = RestaurantListPendingViewSet.as_view({
+    'get': 'list'
+})
+
 urlpatterns = [
     path('', restaurants_list, name='restaurants-list'),
+    path(
+        'pending/',
+        restaurants_list_pending,
+        name='restaurants-list-pending'
+    ),
     path('<int:pk>/', restaurants_detail, name='restaurants-detail'),
     path('cities/', cities_list, name='cities-list'),
     path('cities/<int:pk>/', cities_detail, name='cities-detail'),
