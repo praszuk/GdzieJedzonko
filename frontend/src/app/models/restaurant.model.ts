97eba2597a1438d1a13457c3cf81aa947d00da4c
Sebastian Marut <sebastian.marut@op.pl>
2020-01-21 23:19:45 +0100

    Models updated

--- frontend/src/app/models/restaurant.model.ts
+++ frontend/src/app/models/restaurant.model.ts
@@ -3,8 +3,8 @@ import {SimpleUser} from './simple-user';
 export class Restaurant {
   id: number;
   name: string;
-  lat: number;
-  lon: number;
+  lat: string;
+  lon: string;
   city: number;
   website: string;
   address: string;
