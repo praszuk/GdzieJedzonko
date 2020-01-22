4c3238ee54c300b514a88e084a33932f18d2d0c2
Sebastian Marut <sebastian.marut@op.pl>
2020-01-22 20:08:19 +0100

    Added rating and is_approved fields to restaurant model

--- frontend/src/app/models/restaurant.model.ts
+++ frontend/src/app/models/restaurant.model.ts
@@ -1,4 +1,5 @@
 import {SimpleUser} from './simple-user';
+import {Post} from './post.model';
 
 export class Restaurant {
   id: number;
@@ -8,6 +9,10 @@ export class Restaurant {
   city: number;
   website: string;
   address: string;
-  articles: any[];
+  // tslint:disable-next-line:variable-name
+  article_set: Post[];
+  // tslint:disable-next-line:variable-name
+  is_approved: boolean;
   owner: SimpleUser;
+  rating: number;
 }
